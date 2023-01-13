e8f3b55bd46372e3a2b1752f9a2e4843838396f3
MifdhaMilan <mifmilan@gmail.com>
2023-01-13 17:36:33 +0530

    commit

--- src/main/java/com/mifdhamilan/springbootdemo/controller/EmployeeV2Controller.java
+++ src/main/java/com/mifdhamilan/springbootdemo/controller/EmployeeV2Controller.java
@@ -4,10 +4,9 @@ import com.mifdhamilan.springbootdemo.model.Employee;
 import com.mifdhamilan.springbootdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v2/employees")
@@ -20,5 +19,20 @@ public class EmployeeV2Controller {
     @PostMapping
     public Employee saveEmployee(@RequestBody Employee employee){
         return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable String id){
+        return employeeService.getEmployeeById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployeeById(@PathVariable String id){
+        return employeeService.deleteEmployeeById(id);
     }
 }
--- src/main/java/com/mifdhamilan/springbootdemo/service/EmployeeServiceV2Implementation.java
+++ src/main/java/com/mifdhamilan/springbootdemo/service/EmployeeServiceV2Implementation.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceV2Implementation implements EmployeeService{
@@ -31,16 +32,34 @@ public class EmployeeServiceV2Implementation implements EmployeeService{
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        List<EmployeeEntity>  employeeEntityList =
+                employeeRepository.findAll();
+
+        List<Employee> employees
+                = employeeEntityList
+                .stream()
+                .map(employeeEntity -> {
+                    Employee employee = new Employee();
+                    BeanUtils.copyProperties(employeeEntity, employee);
+                    return employee;
+                })
+                .collect(Collectors.toList());
+        return employees;
     }
 
     @Override
     public Employee getEmployeeById(String id) {
-        return null;
+        EmployeeEntity employeeEntity
+                = employeeRepository.findById(id).get();
+
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
     }
 
     @Override
     public String deleteEmployeeById(String id) {
-        return null;
+        employeeRepository.deleteById(id);
+        return "Employee deleted with id: " + id;
     }
 }
